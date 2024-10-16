9e5926fd61aca544e682f81f0800bb0a9ea7b872
Dan George <dangeorge35@gmail.com>
2024-10-16 12:32:01 +0100

    Updated

--- src/libs/utils/app.utility.ts
+++ src/libs/utils/app.utility.ts
@@ -1,81 +1,81 @@
-import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import fs from 'fs';
-import axios from 'axios';
-import { Fields } from 'formidable';
-import dotenv from 'dotenv';
-import sharp from 'sharp';
+import nodemailer from 'nodemailer'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import fs from 'fs'
+import axios from 'axios'
+import { type Fields } from 'formidable'
+import dotenv from 'dotenv'
+import sharp from 'sharp'
 
-dotenv.config();
+dotenv.config()
 
 const Authorization = (req: any, res: any, next: any): void => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-  if (!token) return res.status(401).send('Not Authorized');
+  const authHeader = req.headers.authorization
+  const token = authHeader?.split(' ')[1]
+  if (!token) return res.status(401).send('Not Authorized')
 
-  const JWT_KEY: string = process.env.jwtkey as string;
+  const JWT_KEY: string = process.env.jwtkey as string
   jwt.verify(token, JWT_KEY, (err: any, user: any) => {
-    if (err) return res.status(401).send('Invalid Token');
-    req.user = user;
-    next();
-  });
-};
+    if (err) return res.status(401).send('Invalid Token')
+    req.user = user
+    next()
+  })
+}
 
 const GenerateToken = (data: any): string => {
-  const JWT_SECRET: string = process.env.jwtkey as string;
-  return jwt.sign({ data }, JWT_SECRET, { expiresIn: '30d' });
-};
+  const JWT_SECRET: string = process.env.jwtkey as string
+  return jwt.sign({ data }, JWT_SECRET, { expiresIn: '30d' })
+}
 
 const CheckPassword = async (password: string, hash: string): Promise<boolean> => {
-  return await bcrypt.compare(password, hash);
-};
+  return await bcrypt.compare(password, hash)
+}
 
 const EncryptPassword = async (password: string): Promise<string> => {
   try {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10
+    return await bcrypt.hash(password, saltRounds)
   } catch (error) {
-    console.error('Error encrypting password:', error);
-    return password;
+    console.error('Error encrypting password:', error)
+    return password
   }
-};
+}
 
 const SendMailJS = async (templateID: string, templateParams: any): Promise<void> => {
   const options = {
     service_id: process.env.EmailJS_service_id,
     template_id: templateID,
     user_id: process.env.EmailJS_user_id,
-    template_params: templateParams,
-  };
+    template_params: templateParams
+  }
 
   const config = {
     method: 'post',
     url: 'https://api.emailjs.com/api/v1.0/email/send',
     data: JSON.stringify(options),
     headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+      'Content-Type': 'application/json'
+    }
+  }
 
   try {
-    const response = await axios(config);
-    console.log(JSON.stringify(response.data));
+    const response = await axios(config)
+    console.log(JSON.stringify(response.data))
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
-const SendMail = async (mail: { to_email: string; to_name: string; subject: string; message: string }): Promise<void> => {
+const SendMail = async (mail: { to_email: string, to_name: string, subject: string, message: string }): Promise<void> => {
   const transporter = nodemailer.createTransport({
     host: process.env.EmailHost,
     port: 465,
     secure: true, // Use secure connection (TLS/SSL)
     auth: {
       user: process.env.EmailUser,
-      pass: process.env.EmailPassword,
-    },
-  });
+      pass: process.env.EmailPassword
+    }
+  })
 
   const htmlMessage = `
 <div style="background-color: #479eff;">
@@ -88,102 +88,101 @@ const SendMail = async (mail: { to_email: string; to_name: string; subject: stri
 
       <br>
       <div>
-  </div>`;
+  </div>`
 
   const mailOptions = {
     from: '"DOCARE" <docare@posaccountant.com>',
     to: mail.to_email,
     subject: mail.subject,
-    html: htmlMessage,
-  };
+    html: htmlMessage
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Error:', error.message);
+      console.error('Error:', error.message)
     } else {
-      console.log('Email sent:', info.response);
+      console.log('Email sent:', info.response)
     }
-  });
-};
+  })
+}
 
 const adjustFieldsToValue = (fieldsObject: Fields): Record<string, string> => {
-  const adjustedFields: Record<string, string> = {};
+  const adjustedFields: Record<string, string> = {}
 
   for (const fieldName in fieldsObject) {
     if (fieldsObject.hasOwnProperty(fieldName)) {
-      const fieldValue = fieldsObject[fieldName]?.[0] ?? ''; // Using optional chaining and nullish coalescing
-      adjustedFields[fieldName] = fieldValue;
+      const fieldValue = fieldsObject[fieldName]?.[0] ?? '' // Using optional chaining and nullish coalescing
+      adjustedFields[fieldName] = fieldValue
     }
   }
-  return adjustedFields;
-};
+  return adjustedFields
+}
 
 const RenameUploadFile = (uploadedfile: any, filename: string): string => {
-  const oldPath = uploadedfile.filepath;
-  const extension = uploadedfile.originalFilename.substring(uploadedfile.originalFilename.lastIndexOf('.'));
-  const newPath = `.${filename}${extension}`;
-  const publicPath = `${process.env.DOMAIN}/${process.env.NODE_ENV}${filename}${extension}`;
-  fs.copyFileSync(oldPath, newPath);
-  fs.unlinkSync(oldPath);
-  return publicPath;
-};
+  const oldPath = uploadedfile.filepath
+  const extension = uploadedfile.originalFilename.substring(uploadedfile.originalFilename.lastIndexOf('.'))
+  const newPath = `.${filename}${extension}`
+  const publicPath = `${process.env.DOMAIN}/${process.env.NODE_ENV}${filename}${extension}`
+  fs.copyFileSync(oldPath, newPath)
+  fs.unlinkSync(oldPath)
+  return publicPath
+}
 
 const ProcessUploadImage = async (uploadedfile: any, filename: string): Promise<string> => {
-  const oldPath = uploadedfile.filepath;
-  const extension = uploadedfile.originalFilename.substring(uploadedfile.originalFilename.lastIndexOf('.'));
-  const newPath = `.${filename}${extension}`;
+  const oldPath = uploadedfile.filepath
+  const extension = uploadedfile.originalFilename.substring(uploadedfile.originalFilename.lastIndexOf('.'))
+  const newPath = `.${filename}${extension}`
 
-  const image = sharp(oldPath);
-  const metadata = await image.metadata();
+  const image = sharp(oldPath)
+  const metadata = await image.metadata()
 
   if (metadata.width && metadata.height) {
-    const newWidth = Math.round(metadata.width * 0.5);
-    const newHeight = Math.round(metadata.height * 0.5);
+    const newWidth = Math.round(metadata.width * 0.5)
+    const newHeight = Math.round(metadata.height * 0.5)
 
-    await image.resize(newWidth, newHeight).toFile(newPath);
-    const publicPath = `${process.env.DOMAIN}/${process.env.NODE_ENV}${filename}${extension}`;
+    await image.resize(newWidth, newHeight).toFile(newPath)
+    const publicPath = `${process.env.DOMAIN}/${process.env.NODE_ENV}${filename}${extension}`
 
     // fs.unlink(oldPath, (err) => {
     //   if (err){console.log(err)}else{
     //   console.log('deleted');}
     // });
-    return publicPath;
+    return publicPath
   }
 
-  throw new Error('Unable to retrieve image dimensions');
-};
-
-const getUIDfromDate = (prefix = ''): string => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const second = date.getSeconds().toString().padStart(2, '0');
-  const random = (Math.floor(Math.random() * 9) + 1).toString();
-  const uniqueNumber = `${year}${month}${day}${hour}${minute}${second}${random.substring(0, 2)}`;
-
-  return prefix ? prefix + uniqueNumber : `IDN${uniqueNumber}`;
-};
-
-
-function generateOTP() {
-  const min = 100000;
-  const max = 999999;
-  // Generate a random value between min and max (inclusive)
-  const otp = Math.floor(Math.random() * (max - min + 1)) + min;
-  return otp;
+  throw new Error('Unable to retrieve image dimensions')
 }
 
-    const SendVerifyToken = async (data:any,token:number) => {
-            // send mail
-      const mail = {
-        to_email: data.Email,
-        to_name: data.FirstName,
-        subject: 'Welcome to DOCARE Health Support!',
-        message: `
-Thank you for expressing interest In Docare. We are thrilled to have you on board as a potential doctor in our exciting platform.<br>
+const getUIDfromDate = (prefix = ''): string => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0') // Months are zero-based
+  const day = date.getDate().toString().padStart(2, '0')
+  const hour = date.getHours().toString().padStart(2, '0')
+  const minute = date.getMinutes().toString().padStart(2, '0')
+  const second = date.getSeconds().toString().padStart(2, '0')
+  const random = (Math.floor(Math.random() * 9) + 1).toString()
+  const uniqueNumber = `${year}${month}${day}${hour}${minute}${second}${random.substring(0, 2)}`
+
+  return prefix ? prefix + uniqueNumber : `IDN${uniqueNumber}`
+}
+
+function generateOTP () {
+  const min = 100000
+  const max = 999999
+  // Generate a random value between min and max (inclusive)
+  const otp = Math.floor(Math.random() * (max - min + 1)) + min
+  return otp
+}
+
+const SendVerifyToken = async (data: any, token: number) => {
+  // send mail
+  const mail = {
+    to_email: data.Email,
+    to_name: data.FirstName,
+    subject: 'Welcome to DOCARE Health Support!',
+    message: `
+Thank you for expressing interest In Docare. We are thrilled to have you on board in our exciting platform.<br>
 <br> please proceed to your account verification  with the OTP below :
  <br><br><b style='font-size: 30px;font-weight: 700;padding: 15px 35px;display: inline-block;background-color: #d6ecff;border-radius: 10px;'>${token}</b>
 <br>
@@ -194,32 +193,27 @@ Best regards,<br>
 DOCARE SUPPORT
 <br>
 `
-      };
+  }
 
-       await SendMail(mail)
+  await SendMail(mail)
+}
 
-    }
-
-
-
-  const SendAccountVerified = async (data:any) => {
-            // send mail
-      const mail = {
-        to_email: data.Email,
-        to_name: data.FirstName,
-        subject: 'Email Successfully Verified!',
-        message: `
+const SendAccountVerified = async (data: any) => {
+  // send mail
+  const mail = {
+    to_email: data.Email,
+    to_name: data.FirstName,
+    subject: 'Email Successfully Verified!',
+    message: `
 Your email is successfully verified. You can login now!<br><br>
 Best regards,<br>
 DOCARE SUPPORT
 <br>
 `
-      };
+  }
 
-       await SendMail(mail)
-
-    }
-
+  await SendMail(mail)
+}
 
 export {
   SendAccountVerified,
@@ -235,4 +229,4 @@ export {
   SendMailJS,
   ProcessUploadImage,
   generateOTP
-};
+}
